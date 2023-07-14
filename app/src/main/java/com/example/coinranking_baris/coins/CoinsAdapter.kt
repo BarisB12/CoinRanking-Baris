01f0938d9acbc7e3566a58347a37df161fa3e48d
Baris Balli <barisballi122@gmail.com>
2023-07-14 15:18:44 +0300

    Uptade CoinsAdapter - Uptade CoinsDetailAdapter

--- app/src/main/java/com/example/coinranking_baris/coins/CoinsAdapter.kt
+++ app/src/main/java/com/example/coinranking_baris/coins/CoinsAdapter.kt
@@ -2,11 +2,13 @@ package com.example.coinranking_baris.coins
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.coinranking_baris.databinding.CoinsItemBinding
 import com.example.coinranking_baris.model.Coin
 import com.example.coinranking_baris.model.Coins
+import com.example.coinranking_baris.ui.CoinsFragmentDirections
 
 class CoinsAdapter(
     private var coinList: List<Coin> = listOf()
@@ -40,6 +42,11 @@ class CoinsAdapter(
         Glide.with(item.buttonRound)
             .load(coin.iconUrl)
             .into(item.buttonRound)
+
+        item.root.setOnClickListener {
+            val direction = CoinsFragmentDirections.actionCoinsFragmentToDetailCoinsFragment(coin)
+            Navigation.findNavController(it).navigate(direction)
+        }
     }
     override fun getItemCount(): Int {
         return coinList.size
