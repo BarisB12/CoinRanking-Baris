13ca2e164ec7dcb1d213079d11a3b9a62e4ec28f
Baris Balli <barisballi122@gmail.com>
2023-07-23 00:20:14 +0300

    Uptade CoinsDetailFragment

--- app/src/main/java/com/example/coinranking_baris/uidetail/CoinsDetailFragment.kt
+++ app/src/main/java/com/example/coinranking_baris/uidetail/CoinsDetailFragment.kt
@@ -1,22 +1,32 @@
 package com.example.coinranking_baris.uidetail
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.coinranking_baris.R
 
 import com.example.coinranking_baris.databinding.FragmentDetailCoinsBinding
+import com.example.coinranking_baris.detailcoins.CoinsHistoryViewModel
+import com.example.coinranking_baris.detailcoins.LineChartAdapter
 import com.example.coinranking_baris.model.Coin
 import java.util.*
 
 class CoinsDetailFragment : Fragment() {
     private lateinit var binding: FragmentDetailCoinsBinding
     private val args by navArgs<CoinsDetailFragmentArgs>()
+    private lateinit var adapter: LineChartAdapter
+    private lateinit var recyclerView: RecyclerView
+
+    private val viewModel: CoinsHistoryViewModel by viewModels()
 
     private lateinit var selectedCoin: Coin
     private var isClicked = false
@@ -26,8 +36,11 @@ class CoinsDetailFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentDetailCoinsBinding.inflate(inflater)
+        binding = FragmentDetailCoinsBinding.inflate(inflater, container, false)
         selectedCoin = args.coin
+
+        recyclerView = binding.recyclerViewLineChart
+        adapter = LineChartAdapter()
 
         val detailPrice = "%.5f".format(Locale.ENGLISH, selectedCoin.price.toDouble())
         binding.textViewDetailPrice.text = "$${detailPrice}"
@@ -50,6 +63,15 @@ class CoinsDetailFragment : Fragment() {
             binding.textMarketDetailCap.setTextColor(ContextCompat.getColor(binding.root.context, R.color.green))
         } else {
             binding.textMarketDetailCap.setTextColor(ContextCompat.getColor(binding.root.context, R.color.red))
+        }
+
+        val layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = adapter
+
+        viewModel.historyCoins.observe(viewLifecycleOwner) { historyList ->
+            Log.d("CoinsDetailFragment", "History list: $historyList")
+            adapter.submitList(historyList)
         }
 
         binding.imageViewBell.setOnClickListener {
