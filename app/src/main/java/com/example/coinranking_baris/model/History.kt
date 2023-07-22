69c60c267b1c11b163e922e08ceee676293fbe44
Baris Balli <barisballi122@gmail.com>
2023-07-23 00:16:40 +0300

    Uptade data class History

--- app/src/main/java/com/example/coinranking_baris/model/History.kt
+++ app/src/main/java/com/example/coinranking_baris/model/History.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 
 data class History(
     @SerializedName("data")
-    val `data`: DataX,
+    val data: DataX,
     @SerializedName("status")
     val status: String
 )
