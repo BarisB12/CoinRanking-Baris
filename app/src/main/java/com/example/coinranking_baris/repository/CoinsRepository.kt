e53095987a7dfe2c9e83c3bcb303ed7629b24cff
Baris Balli <barisballi122@gmail.com>
2023-07-23 00:24:01 +0300

    Rename CoinRepository

--- app/src/main/java/com/example/coinranking_baris/repository/CoinsRepository.kt
+++ app/src/main/java/com/example/coinranking_baris/repository/CoinsRepository.kt
@@ -1,12 +1,12 @@
 package com.example.coinranking_baris.repository
 
-import com.example.coinranking_baris.api.CoinService
+import com.example.coinranking_baris.api.CoinsService
 import com.example.coinranking_baris.api.Service
 import com.example.coinranking_baris.model.Coins
 
 
-class CoinRepository(
-    private var service: CoinService = Service.getCoinService()
+class CoinsRepository(
+    private var service: CoinsService = Service.getCoinsService()
 ){
         suspend fun getAllCoins(): Coins {
             return this.service.getAllCoins()
