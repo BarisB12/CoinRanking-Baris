90997d2759838145efecaee50498399ac93bc7c4
Baris Balli <barisballi122@gmail.com>
2023-07-25 13:50:51 +0300

    Uptade CoinsDetailViewModel

--- app/src/main/java/com/example/coinranking_baris/detailcoins/CoinsDetailViewModel.kt
+++ app/src/main/java/com/example/coinranking_baris/detailcoins/CoinsDetailViewModel.kt
@@ -2,36 +2,34 @@ package com.example.coinranking_baris.detailcoins
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.coinranking_baris.api.CoinsService
 import com.example.coinranking_baris.model.Coin
 import com.example.coinranking_baris.model.HistoryX
 import com.example.coinranking_baris.repository.CoinsDetailRepository
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
 
-class CoinsDetailViewModel : ViewModel() {
-    private val coinsHistoryRepository = CoinsDetailRepository()
-    private val _historyCoins = MutableLiveData<List<HistoryX>>()
+    class CoinsDetailViewModel : ViewModel() {
+        private val coinsHistoryRepository = CoinsDetailRepository()
+        private val _historyCoins = MutableLiveData<List<HistoryX>>()
+        private val _selectedCoin = MutableLiveData<Coin>()
+        val selectedCoin: LiveData<Coin> get() = _selectedCoin
+        val historyCoins: LiveData<List<HistoryX>> get() = _historyCoins
 
-    private val _selectedCoin = MutableLiveData<Coin>()
-    val selectedCoin: LiveData<Coin> get() = _selectedCoin
-
-    val historyCoins: LiveData<List<HistoryX>> get() = _historyCoins
-    fun setSelectedCoin(coin: Coin) {
-        _selectedCoin.value = coin
-    }
-    init {
-        val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-            println("Handle Exception $throwable")
-        }
-        viewModelScope.launch(exceptionHandler) {
-            try {
-                val historyList = coinsHistoryRepository.getAllHistoryCoins()
-                    _historyCoins.value = historyList.data.history
-            } catch (e: Exception) {
-                println("Handle Exception $e")
+        init {
+            val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+                println("Handle Exception $throwable")
+            }
+            viewModelScope.launch(exceptionHandler) {
+                try {
+                    val historyList = coinsHistoryRepository.getAllHistoryCoins()
+                        _historyCoins.value = historyList.data.history
+                } catch (e: Exception) {
+                    println("Handle Exception $e")
+                }
             }
         }
-    }
-}
+        }
