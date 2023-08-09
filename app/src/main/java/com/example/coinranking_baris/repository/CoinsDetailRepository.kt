3a236655a4812a5a04f278d4152dc3641eb6f011
Baris <107356112+BarisB12@users.noreply.github.com>
2023-08-09 18:18:50 +0300

    Update CoinsDetailRepository

--- app/src/main/java/com/example/coinranking_baris/repository/CoinsDetailRepository.kt
+++ app/src/main/java/com/example/coinranking_baris/repository/CoinsDetailRepository.kt
@@ -4,15 +4,13 @@ import android.util.Log
 import com.example.coinranking_baris.api.CoinsService
 import com.example.coinranking_baris.api.Service
 import com.example.coinranking_baris.model.Coins
-import com.example.coinranking_baris.model.History
 
 class CoinsDetailRepository {
     private var detailService: CoinsService = Service.getCoinsService()
 
     suspend fun getAllDetailCoins():Coins {
         try {
-            val detailList = this.detailService.getAllDetailCoins()
-            return detailList
+            return detailService.getAllDetailCoins()
 
         } catch (e: Exception) {
             Log.e("CoinsHistoryRepository", "getAllHistoryCoins: Error - ${e.message}", e)
