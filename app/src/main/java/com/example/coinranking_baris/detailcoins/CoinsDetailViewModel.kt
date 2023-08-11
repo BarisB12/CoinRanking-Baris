502049155b27fa8884a5af4c2fafc3b43abc5e47
Baris <107356112+BarisB12@users.noreply.github.com>
2023-08-11 15:47:54 +0300

    Add CoinDetailResponse - Update CoinsDetailViewModel

--- app/src/main/java/com/example/coinranking_baris/detailcoins/CoinsDetailViewModel.kt
+++ app/src/main/java/com/example/coinranking_baris/detailcoins/CoinsDetailViewModel.kt
@@ -5,23 +5,33 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.coinranking_baris.model.Coin
+import com.example.coinranking_baris.model.CoinDetailResponse
 import com.example.coinranking_baris.repository.CoinsDetailRepository
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
 
     class CoinsDetailViewModel : ViewModel() {
         private val coinsDetailRepository = CoinsDetailRepository()
-        private val _detailCoins = MutableLiveData<List<Coin>>()
-        val detailCoins: LiveData<List<Coin>> get() = _detailCoins
+        private val _coinDetailLiveData = MutableLiveData<CoinDetailResponse.Data.CoinDetail?>()
+        val coinDetailLiveData: LiveData<CoinDetailResponse.Data.CoinDetail?> get() = _coinDetailLiveData
 
         init {
+
+        }
+
+        fun getDetail(uuid: String) {
             val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
                 println("Handle Exception $throwable")
             }
             viewModelScope.launch(exceptionHandler) {
                 try {
-                    val detailList = coinsDetailRepository.getAllDetailCoins()
-                    _detailCoins.value = detailList.data.coin
+                    val coinDetailResponse = coinsDetailRepository.getCoinDetail(uuid)
+                    if (coinDetailResponse.status == "success") {
+                        _coinDetailLiveData.value = coinDetailResponse.result?.coin
+
+                    } else {
+                        print("status is not success")
+                    }
                 } catch (e: Exception) {
                     println("Handle Exception $e")
                 }
---
+++ app/src/main/java/com/example/coinranking_baris/model/CoinDetailResponse.kt
@@ -0,0 +1,107 @@
+package com.example.coinranking_baris.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class CoinDetailResponse(
+    @SerializedName("data")
+    val result: Data?,
+    @SerializedName("status")
+    val status: String?
+) {
+    data class Data(
+        @SerializedName("coin")
+        val coin: CoinDetail?
+    ) {
+        data class CoinDetail(
+            @SerializedName("allTimeHigh")
+            val allTimeHigh: AllTimeHigh?,
+            @SerializedName("btcPrice")
+            val btcPrice: String?,
+            @SerializedName("change")
+            val change: String?,
+            @SerializedName("coinrankingUrl")
+            val coinrankingUrl: String?,
+            @SerializedName("color")
+            val color: String?,
+            @SerializedName("description")
+            val description: String?,
+            @SerializedName("fullyDilutedMarketCap")
+            val fullyDilutedMarketCap: String?,
+            @SerializedName("24hVolume")
+            val hVolume: String?,
+            @SerializedName("iconUrl")
+            val iconUrl: String?,
+            @SerializedName("links")
+            val links: List<Link?>?,
+            @SerializedName("listedAt")
+            val listedAt: Int?,
+            @SerializedName("lowVolume")
+            val lowVolume: Boolean?,
+            @SerializedName("marketCap")
+            val marketCap: String?,
+            @SerializedName("name")
+            val name: String?,
+            @SerializedName("notices")
+            val notices: List<Notice?>?,
+            @SerializedName("numberOfExchanges")
+            val numberOfExchanges: Int?,
+            @SerializedName("numberOfMarkets")
+            val numberOfMarkets: Int?,
+            @SerializedName("price")
+            val price: String?,
+            @SerializedName("priceAt")
+            val priceAt: Int?,
+            @SerializedName("rank")
+            val rank: Int?,
+            @SerializedName("sparkline")
+            val sparkline: List<String?>?,
+            @SerializedName("supply")
+            val supply: Supply?,
+            @SerializedName("symbol")
+            val symbol: String?,
+            @SerializedName("tags")
+            val tags: List<String?>?,
+            @SerializedName("uuid")
+            val uuid: String?,
+            @SerializedName("websiteUrl")
+            val websiteUrl: String?
+        ) {
+            data class AllTimeHigh(
+                @SerializedName("price")
+                val price: String?,
+                @SerializedName("timestamp")
+                val timestamp: Int?
+            )
+
+            data class Link(
+                @SerializedName("name")
+                val name: String?,
+                @SerializedName("type")
+                val type: String?,
+                @SerializedName("url")
+                val url: String?
+            )
+
+            data class Notice(
+                @SerializedName("type")
+                val type: String?,
+                @SerializedName("value")
+                val value: String?
+            )
+
+            data class Supply(
+                @SerializedName("circulating")
+                val circulating: String?,
+                @SerializedName("confirmed")
+                val confirmed: Boolean?,
+                @SerializedName("max")
+                val max: String?,
+                @SerializedName("supplyAt")
+                val supplyAt: Int?,
+                @SerializedName("total")
+                val total: String?
+            )
+        }
+    }
+}
