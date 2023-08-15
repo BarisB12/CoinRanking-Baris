402fc177f8791caf351d02164dab547530cbdc68
Baris <107356112+BarisB12@users.noreply.github.com>
2023-08-15 15:29:34 +0300

    Update CoinsDetailViewModel

--- app/src/main/java/com/example/coinranking_baris/detailcoins/CoinsDetailViewModel.kt
+++ app/src/main/java/com/example/coinranking_baris/detailcoins/CoinsDetailViewModel.kt
@@ -4,37 +4,43 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.coinranking_baris.model.Coin
 import com.example.coinranking_baris.model.CoinDetailResponse
+import com.example.coinranking_baris.repository.FavoritesRepository
 import com.example.coinranking_baris.repository.CoinsDetailRepository
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
 
-    class CoinsDetailViewModel : ViewModel() {
-        private val coinsDetailRepository = CoinsDetailRepository()
-        private val _coinDetailLiveData = MutableLiveData<CoinDetailResponse.Data.CoinDetail?>()
-        val coinDetailLiveData: LiveData<CoinDetailResponse.Data.CoinDetail?> get() = _coinDetailLiveData
+class CoinsDetailViewModel : ViewModel() {
+    private val coinsDetailRepository = CoinsDetailRepository()
+    private val _coinDetailLiveData = MutableLiveData<CoinDetailResponse.Data.CoinDetail?>()
+    private val checkFavoritedRepository = FavoritesRepository()
+    val coinDetailLiveData: LiveData<CoinDetailResponse.Data.CoinDetail?> get() = _coinDetailLiveData
 
-        init {
+    init {
 
+    }
+
+    fun getDetail(uuid: String) {
+        val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+            println("Handle Exception $throwable")
         }
-
-        fun getDetail(uuid: String) {
-            val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-                println("Handle Exception $throwable")
-            }
-            viewModelScope.launch(exceptionHandler) {
-                try {
-                    val coinDetailResponse = coinsDetailRepository.getCoinDetail(uuid)
-                    if (coinDetailResponse.status == "success") {
-                        _coinDetailLiveData.value = coinDetailResponse.result?.coin
-
-                    } else {
-                        print("status is not success")
+        viewModelScope.launch(exceptionHandler) {
+            try {
+                val coinDetailResponse = coinsDetailRepository.getCoinDetail(uuid)
+                if (coinDetailResponse.status == "success") {
+                    val coinDetail = coinDetailResponse.result?.coin
+                    if (coinDetail != null) {
+                        val isFavorite = checkFavoritedRepository.checkFavoritedCoin(coinDetail.name ?: "")
+                        coinDetail.isFav = isFavorite
+                        _coinDetailLiveData.value = coinDetail
                     }
-                } catch (e: Exception) {
-                    println("Handle Exception $e")
+
+                } else {
+                    print("status is not success")
                 }
+            } catch (e: Exception) {
+                println("Handle Exception $e")
             }
         }
-        }
+    }
+}
